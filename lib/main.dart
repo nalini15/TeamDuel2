fa5a2daa714a23d6913faabe96d1202c87b357ba
nalini15 <58021025+nalini15@users.noreply.github.com>
2021-04-07 15:21:41 +0530

    push notification

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:admob_flutter/admob_flutter.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 import 'package:winx/providers/cricketStates.dart';
@@ -76,30 +77,138 @@ class _MyAppState extends State<MyApp> {
 
   String _message = '';
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-  _register() {
-    _firebaseMessaging.getToken().then((token) => print(token));
-  }
-
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
   @override
   void initState() {
     super.initState();
-    getMessage();
-    _register();
-  }
+    var initializationSettingsAndroid =
+        new AndroidInitializationSettings('@mipmap/ic_launcher');
 
-  void getMessage() {
+    var initializationSettingsIOS = new IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidRecieveLocalNotification);
+
+    var initializationSettings = new InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+
     _firebaseMessaging.configure(
-        onMessage: (Map<String, dynamic> message) async {
-      print('on message $message');
-      setState(() => _message = message["notification"]["title"]);
-    }, onResume: (Map<String, dynamic> message) async {
-      print('on resume $message');
-      setState(() => _message = message["notification"]["title"]);
-    }, onLaunch: (Map<String, dynamic> message) async {
-      print('on launch $message');
-      setState(() => _message = message["notification"]["title"]);
+      onMessage: (Map<String, dynamic> message) {
+        print('on message ${message}');
+        // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+        displayNotification(message);
+        // _showItemDialog(message);
+      },
+      onResume: (Map<String, dynamic> message) {
+        print('on resume $message');
+      },
+      onLaunch: (Map<String, dynamic> message) {
+        print('on launch $message');
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print(token);
     });
   }
+
+  Future displayNotification(Map<String, dynamic> message) async {
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'channelid', 'flutterfcm', 'your channel description',
+        importance: Importance.Max, priority: Priority.High);
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      message['notification']['title'],
+      message['notification']['body'],
+      platformChannelSpecifics,
+      payload: 'hello',
+    );
+  }
+
+  Future onSelectNotification(String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+    // await Fluttertoast.showToast(
+    //     msg: "Notification Clicked",
+    //     toastLength: Toast.LENGTH_SHORT,
+    //     gravity: ToastGravity.BOTTOM,
+    //     timeInSecForIos: 1,
+    //     backgroundColor: Colors.black54,
+    //     textColor: Colors.white,
+    //     fontSize: 16.0
+    // );
+    /*Navigator.push(
+      context,
+      new MaterialPageRoute(builder: (context) => new SecondScreen(payload)),
+    );*/
+  }
+
+  Future onDidRecieveLocalNotification(
+      int id, String title, String body, String payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    // showDialog(
+    //   context: context,
+    //   builder: (BuildContext context) => new CupertinoAlertDialog(
+    //         title: new Text(title),
+    //         content: new Text(body),
+    //         actions: [
+    //           CupertinoDialogAction(
+    //             isDefaultAction: true,
+    //             child: new Text('Ok'),
+    //             onPressed: () async {
+    //               Navigator.of(context, rootNavigator: true).pop();
+    //               await Fluttertoast.showToast(
+    //                   msg: "Notification Clicked",
+    //                   toastLength: Toast.LENGTH_SHORT,
+    //                   gravity: ToastGravity.BOTTOM,
+    //                   timeInSecForIos: 1,
+    //                   backgroundColor: Colors.black54,
+    //                   textColor: Colors.white,
+    //                   fontSize: 16.0
+    //               );
+    //             },
+    //           ),
+    //         ],
+    //       ),
+    // );
+  }
+  // final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  // _register() {
+  //   _firebaseMessaging.getToken().then((token) => print(token));
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getMessage();
+  //   _register();
+  // }
+
+  // void getMessage() {
+  //   _firebaseMessaging.configure(
+  //       onMessage: (Map<String, dynamic> message) async {
+  //     print('on message $message');
+  //     setState(() => _message = message["notification"]["title"]);
+  //   }, onResume: (Map<String, dynamic> message) async {
+  //     print('on resume $message');
+  //     setState(() => _message = message["notification"]["title"]);
+  //   }, onLaunch: (Map<String, dynamic> message) async {
+  //     print('on launch $message');
+  //     setState(() => _message = message["notification"]["title"]);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
